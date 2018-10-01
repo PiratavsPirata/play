5325c0254686346a1674f8fc4deeb7c2c574f858
redirecionadorga <39850106+redirecionadorga@users.noreply.github.com>
2018-09-30 21:53:43 -0300

    Add files via upload

---
+++ assets/css/player6.php
@@ -0,0 +1,150 @@
+/* PLAYBACK */
+.jw-state-idle .jw-svg-icon-play path {	
+	display: none;
+}
+.jw-state-idle .jw-svg-icon-play {
+	background-image: url('assets/play-idle.svg');
+	background-size: contain;
+	background-repeat: no-repeat;
+    opacity: 0.6;
+}
+.jw-state-idle .jw-display-icon-container:hover .jw-svg-icon-play {
+    opacity: 1.0;
+}
+.jw-state-paused .jw-svg-icon-play path {	
+	display: none;
+}
+.jw-state-paused .jw-svg-icon-play {
+	background-image: url('assets/play.svg');
+	background-size: contain;
+	background-repeat: no-repeat;
+    opacity: 0.6;
+}
+.jw-state-paused .jw-icon-playback:hover .jw-svg-icon-play {
+    opacity: 1.0;
+}
+.jw-svg-icon-pause path {	
+	display: none;
+}
+.jw-svg-icon-pause {
+	background-image: url('assets/pause.svg');
+	background-size: contain;
+	background-repeat: no-repeat;
+    opacity: 0.6;
+}
+.jw-icon-playback:hover .jw-svg-icon-pause {
+    opacity: 1.0;
+}
+
+
+/* NEXT */
+.jw-svg-icon-next path {	
+	display: none;
+}
+.jw-svg-icon-next {
+	background-image: url('assets/next.svg');
+	background-size: contain;
+	background-repeat: no-repeat;
+    opacity: 0.6;
+}
+.jw-icon-next:hover .jw-svg-icon-next {
+    opacity: 1.0;
+}
+
+
+/* CLOSED CAPTIONS */
+.jw-svg-icon-cc-off path {	
+	display: none;
+}
+.jw-svg-icon-cc-off {
+	background-image: url('assets/captions.svg');
+	background-size: contain;
+	background-repeat: no-repeat;
+    opacity: 0.6;
+}
+.jw-icon-cc-off:hover .jw-svg-icon-cc-off {
+    opacity: 1.0;
+}
+
+
+/* PLAYLIST */
+.jw-svg-icon-playlist path {	
+	display: none;
+}
+.jw-svg-icon-playlist {
+	background-image: url('assets/playlist.svg');
+	background-size: contain;
+	background-repeat: no-repeat;
+    opacity: 0.6;
+}
+.jw-playlist-btn:hover .jw-svg-icon-playlist {
+    opacity: 1.0;
+}
+
+
+/* SETTINGS MENU */
+.jw-svg-icon-settings path {	
+	display: none;
+}
+.jw-svg-icon-settings {
+	background-image: url('assets/001-language-01.svg');
+	background-size: contain;
+	background-repeat: no-repeat;
+    opacity: 0.6;
+}
+.jw-icon-settings:hover .jw-svg-icon-settings {
+    opacity: 1.0;
+}
+.jw-svg-icon-quality-100 path {	
+	display: none;
+}
+.jw-svg-icon-quality-100 {
+	background-image: url('assets/002-music-01.svg');
+	background-size: contain;
+	background-repeat: no-repeat;
+    opacity: 0.6;
+}
+.jw-settings-quality:hover .jw-svg-icon-quality-100 {
+    opacity: 1.0;
+}
+.jw-svg-icon-close path {	
+	display: none;
+}
+.jw-svg-icon-close {
+	background-image: url('assets/close.svg');
+	background-size: contain;
+	background-repeat: no-repeat;
+    opacity: 0.6;
+}
+.jw-settings-close:hover .jw-svg-icon-close {
+    opacity: 1.0;
+}
+
+
+/* FULLSCREEN */
+.jw-svg-icon-fullscreen-on path {	
+	display: none;
+}
+.jw-svg-icon-fullscreen-on {
+	background-image: url('assets/fullscreen-on.svg');
+	background-size: contain;
+	background-repeat: no-repeat;
+    opacity: 0.6;
+}
+.jw-icon-fullscreen:hover .jw-svg-icon-fullscreen-on {
+    opacity: 1.0;
+}
+.jw-svg-icon-fullscreen-off path {	
+	display: none;
+}
+.jw-svg-icon-fullscreen-off {
+	background-image: url('assets/fullscreen-off.svg');
+	background-size: contain;
+	background-repeat: no-repeat;
+    opacity: 0.6;
+}
+.jw-icon-fullscreen:hover .jw-svg-icon-fullscreen-off {
+    opacity: 1.0;
+}
+
+
